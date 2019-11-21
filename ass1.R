f21f579031405eae70edbbb279872364e0d0ac7d
andre <atlantas95@gmail.com>
2019-11-21 16:30:55 +0100

    fine uno

--- ass1.R
+++ ass1.R
@@ -1,5 +1,8 @@
+require(ggplot2)
 dataset <- get(load("/Users/andrea/Desktop/TFCM/keyPressDataWithLaneDeviation.Rdata"))
 dataset <- subset(dataset, typingErrorMadeOnTrial == 0)
+
+
 
 dualSteerFocus <- subset(dataset, partOfExperiment == "dualSteerFocus")
 dualDialFocus <- subset(dataset, partOfExperiment == "dualDialFocus")
@@ -29,6 +32,38 @@ latDevSd <- aggregate(dataset$lanePosition, list(dataset$partOfExperiment), sd, 
 #print(max(unique(dataset$pp)))
 StandErr_Dial <- latDevSd[2] / sqrt(max(unique(dataset$pp)))
 #-------------------------CCCCCCCCCCCCCCCCCCCCc----------------
+
+steer_pp_mean <- aggregate(abs(dualSteerFocus$lanePosition)
+                                , list(participant = dualSteerFocus$pp, keypress = dualSteerFocus$phoneNrLengthAfterKeyPress)
+                                , mean)
+dial_pp_mean <- aggregate(abs(dualDialFocus$lanePosition)
+                          , list(participant = dualDialFocus$pp, keypress = dualDialFocus$phoneNrLengthAfterKeyPress)
+                          , mean)
+
+avg_pp_steer <- aggregate(abs(steer_pp_mean$x), list(keypress = steer_pp_mean$keypress), mean)
+avg_pp_dial <- aggregate(abs(dial_pp_mean$x), list(keypress = dial_pp_mean$keypress), mean)
+
+
+result <- data.frame(avg_pp_dial$keypress, avg_pp_dial$x
+                          , avg_pp_steer$keypress, avg_pp_steer$x)
+
+#can use geom_point instead of geom_line
+gplot <- gplot + geom_line(aes(x = result$avg_pp_dial.keypress
+                                , y = result$avg_pp_dial.x
+                                , colour = "Dialing Focus"), shape = 15)
+gplot <- gplot + geom_line(aes(x = result$avg_pp_steer.keypress
+                                , y = result$avg_pp_steer.x
+                                , colour = "Steering Focus"), shape = 15)
+
+gplot <- gplot + scale_colour_manual(limits=c("Dialing Focus", "Steering Focus"), 
+                                      values=c("red", "blue"))
+
+gplot <- gplot + labs(title="Dual Dial vs Dual Steer Focus Condition", x ="Key Presses", y = "Lane Deviation")
+
+gplot$labels$colour <- "Experimental Conditions"
+plot(gplot)
+
+#------------------DDDDDDDDDDDDDDDDDDD--------------------
 
 
 
